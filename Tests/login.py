1b0eaeb1b64229d7b5f6e2f72ed5289db701fbd2
ademar joaquim <ajoaquim@yahoo.com>
2023-04-02 16:13:08 -0300

    funcionando por chamada de linha comando

--- Tests/login.py
+++ Tests/login.py
@@ -1,22 +1,39 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
-
-s = Service('C:/temp/ws-utilities/chromedriver.exe')
-driver = webdriver.Chrome(service=s)
-driver.maximize_window()
-driver.get('https://us.yahoo.com')
+import unittest
+import time
 
 
-driver.find_element(By.XPATH,'//*[@id="ybarAccountProfile"]/a').click()
-driver.find_element(By.ID, 'login-username').send_keys('ajoaquim@yahoo.com')
-driver.find_element(By.ID, 'login-signin').click()
+class LoginTest(unittest.TestCase):
 
-driver.find_element(By.ID, 'login-passwd').send_keys('MATR@676366')
-driver.find_element(By.ID, 'login-signin').click()
-driver.implicitly_wait(10)
-driver.find_element(By.XPATH, '//*[@id="ybarAccountMenuOpener"]/span').click()
-driver.find_element(By.XPATH, '//*[@id="profile-signout-link"]/span[2]').click()
-driver.implicitly_wait(10)
-driver.quit()
+    @classmethod
+    def setUpClass(cls):
+        s = Service('C:/temp/ws-utilities/chromedriver.exe')
+        cls.driver = webdriver.Chrome(service=s)
+        cls.driver.maximize_window()
+        cls.driver.implicitly_wait(10)
+
+    def test_login_valid(self):
+        self.driver.get('https://us.yahoo.com')
+        self.driver.find_element(By.XPATH,'//*[@id="ybarAccountProfile"]/a').click()
+        self.driver.find_element(By.ID, 'login-username').send_keys('ajoaquim@yahoo.com')
+        self.driver.find_element(By.ID, 'login-signin').click()
+        self.driver.find_element(By.ID, 'login-passwd').send_keys('MATR@676366')
+        self.driver.find_element(By.ID, 'login-signin').click()
+        time.sleep(5)
+        self.driver.find_element(By.XPATH, '//*[@id="ybarAccountMenuOpener"]/span').click()
+        self.driver.find_element(By.XPATH, '//*[@id="profile-signout-link"]/span[2]').click()
+        time.sleep(5)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.close()
+        cls.driver.quit()
+        print('Test Completed!!')
+
+if __name__ == '__main__':
+        unittest.main()
+
+
 
